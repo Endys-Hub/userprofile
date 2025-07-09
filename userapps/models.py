85aa7eab2883ed2846925da8c36676424f0ea857
Endys-Hub <ndudiokehi@gmail.com>
2025-07-09 23:44:07 +0100

    model creation and registration

--- userapps/models.py
+++ userapps/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #first_name, last_name, username, password1, password2, email
+    fullname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    profile_pix = models.ImageField(upload_to='userpix', default='https://cdn-icons-png.flaticon.com/128/456/456212.png')
+
+    def __str__(self):
+        return self.fullname
+
+    
