66c6df2deb456e67630fa5ab21f2281587f96739
Endys-Hub <ndudiokehi@gmail.com>
2025-07-10 04:14:06 +0100

    Registration views, Serializers and URLs

--- userapps/views.py
+++ userapps/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
 # Create your views here.
+
+from rest_framework import status, serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from django.contrib.auth.models import User
+
+from . serializers import UserSerializer, UserProfileSerializer, RegistrationSerializer
+from . models import UserProfile
+
+class RegistrationView(APIView):
+    def post(self,request):
+        try:
+            serializers = RegistrationSerializer(data = request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=status.HTTP_201_CREATED)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"Error":str(e)})
+        
+
