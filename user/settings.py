88e6542dd409e569132d2bbdfa650e02a62f7f9f
Endys-Hub <ndudiokehi@gmail.com>
2025-07-11 04:07:11 +0100

    JWT Removal

--- user/settings.py
+++ user/settings.py
@@ -57,12 +57,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'user.urls'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        ]
-}
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
