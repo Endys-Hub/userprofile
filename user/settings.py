92cb05cbf9b9c869f8aa92e09df80c71afbaa799
Endys-Hub <ndudiokehi@gmail.com>
2025-07-12 03:20:49 +0100

    Settings Updates

--- user/settings.py
+++ user/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
