6555e045a523165abe4aae8fe285af8eec8a9322
Endys-Hub <ndudiokehi@gmail.com>
2025-07-12 17:03:52 +0100

    Settings

--- user/settings.py
+++ user/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://userprofile-a7tt.onrender.com','userprofile-a7tt.onrender.com','http://userprofile-a7tt.onrender.com'] # ['*'] 
 
@@ -144,3 +144,5 @@ EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ['https://userprofile-a7tt.onrender.com']
+
+APPEND_SLASH = True
